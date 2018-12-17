061781513a7ded37907e34a870d591ce9a3997d2
guilhermelameira <gui.l.a@hotmail.com>
2018-12-17 13:42:12 -0800

    Removed old files

--- dbconfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    user: process.env.NODE_ORACLEDB_USER || "ora_k3n0b",
-    password: process.env.NODE_ORACLEDB_PASSWORD || "a20318151",
-    connectString: process.env.NODE_ORACLEDB_CONNECTIONSTRING || "dbhost.ugrad.cs.ubc.ca:1522/ug",
-    externalAuth: process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
-};
